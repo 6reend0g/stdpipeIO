451ae0a0a3ea6622a881e3ca6ab3b41741823766
greendog <xinyu.lyu@qq.com>
2025-02-18 01:33:25 +0800

    edit setup.py for 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stdpipeIO',
-    version='0.1.3',
+    version='0.1.4',
     author='Greendog',
     author_email='x6reend0g@foxmail.com',
     description='A simple decorator for receiving and sending data via the standard pipe',
